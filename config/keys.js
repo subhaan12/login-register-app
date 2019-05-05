4da652fd0388f8e80e5d34ea5c4d2d2040e9299f
Subhaan Vhora <Subhaan.vhora12@gmail.com>
2019-05-04 20:23:14 -0500

    login and register app to view dashboard

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://:'+ encodeURIComponent('') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: 'mongodb+srv://subhaan123:subhaan123@cluster0-wbusq.mongodb.net/test?retryWrites=true'
+};
+
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user
+  })
+);
+
+router.get('/home', (req, res) => res.render('home'));
+
+router.get('/services', (req, res) => res.render('services'));
+
+router.get('/about', (req, res) => res.render('about'));
+
+module.exports = router;
